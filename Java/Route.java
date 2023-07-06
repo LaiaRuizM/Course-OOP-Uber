bb0fdead0336f9e0817dd556e1d969af6c8f8b19
LaiaRuizM <laia.ruizm@hotmail.com>
2023-07-06 10:43:58 +0200

    feat: add Payment and Route classes to JAVA

---
+++ Java/Route.java
@@ -0,0 +1,10 @@
+package Java;
+import java.util.ArrayList;
+
+class Route {
+    Integer id;
+    ArrayList<Double> start;
+    ArrayList<Double> end;
+}
+
+//Double porque las coordenadas son en decimales y son elevados de tamaño.
