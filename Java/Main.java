a4ba2b26e4c91042e07903c65eaad008eada714e
LaiaRuizM <laia.ruizm@hotmail.com>
2023-07-30 17:42:50 +0200

    add two objects with Car class

--- Java/Main.java
+++ Java/Main.java
@@ -1,8 +1,20 @@
-package Java;
+ package Java;
 class Main {
 
+    // two objects (car and car2) from car class:
     public static void main(String[] args) {
         System.out.println("Good morning");
+        Car car = new Car();  // type of object + name of the object = new + constructor method
+        car.license = "AMQ123"; //licence, driver, etc are attributes
+        car.driver = "Andrés Herrera";
+        car.passenger = 4;
+        System.out.println("Car license: " + car.license);
+
+        Car car2 = new Car();  
+        car2.license = "QWE567";
+        car2.driver = "Andrea Herrera";
+        car2.passenger = 3;
+        System.out.println("Car license: " + car2.license);
     }
 }
 
