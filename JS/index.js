c5661f308db9a76271badc6c4370c5985bef644e
LaiaRuizM <laia.ruizm@hotmail.com>
2023-07-31 15:09:05 +0200

    feat: fix Account -> document undefined -> inspect on browser

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 //Equivalent place to the entry point of the application where we will be declaring our objects based on the classes
 //call object car:
-var car = new Car("AAW456", new Account("Andrés Herrera"));
+var car = new Car("AAW456", new Account("Andrés Herrera", "QWE234"));
 car.passenger = 4;
 car.printDataCar();
