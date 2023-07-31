4b5d37ddff9a09a30a2cf0ffe202d1ea21a6d58c
LaiaRuizM <laia.ruizm@hotmail.com>
2023-07-31 16:22:18 +0200

    chore: update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 //Equivalent place to the entry point of the application where we will be declaring our objects based on the classes
 //call object car:
-var car = new Car("AAW456", new Account("Andrés Herrera", "QWE234"));
+let car = new Car("AAW456", new Account("Andrés Herrera", "QWE234")); //To instantiate an object we will continue to use the "new" keyword.
 car.passenger = 4;
 car.printDataCar();
