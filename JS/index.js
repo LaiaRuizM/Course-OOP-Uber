d4f46282c3266e1fe7ab1aa3b69c3c6774af03f9
LaiaRuizM <laia.ruizm@hotmail.com>
2023-07-31 15:01:16 +0200

    feat: add object to index.js

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,5 @@
 //Equivalent place to the entry point of the application where we will be declaring our objects based on the classes
+//call object car:
+var car = new Car("AAW456", new Account("Andrés Herrera"));
+car.passenger = 4;
+car.printDataCar();
