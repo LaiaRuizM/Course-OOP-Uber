5d4bac04effb950affd616326c9f41473b7adada
LaiaRuizM <laia.ruizm@hotmail.com>
2023-08-01 12:11:56 +0200

    feat: add PHP constructor

--- PHP/index.php
+++ PHP/index.php
@@ -1,2 +1,8 @@
 <?php
-    echo 'Hi World';
+require_once "Car.php";
+require_once "Account.php";
+
+$car = new Car('ABC123', new Account('Laia Ruiz', 'LRM27'));
+    $car->printDataCar();
+
+//    echo 'Hi World';
