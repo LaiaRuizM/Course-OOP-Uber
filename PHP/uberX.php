4b80cbe02add7ce0795fbd46ced0ae7aeb9fdecb
LaiaRuizM <laia.ruizm@hotmail.com>
2023-08-01 16:34:34 +0200

    feat: add inherence to PHP

--- PHP/uberX.php
+++ PHP/uberX.php
@@ -1,6 +1,14 @@
 <?php
 class UberX extends Car { //Same syntax as Java
     public $brand;
+    public $model;
+
+    public function __construct($license, Account $driver, $brand, $model){
+        parent::__construct($license, $driver);  //Equivalent to "super" in Java
+        $this->license = $license; //Keyword: this
+        $this->brand = $brand; //New additional data
+        $this->model = $model; //New additional data
+    }
 }
 ?>
 
