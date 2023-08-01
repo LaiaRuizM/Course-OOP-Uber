83a058599f632ae524b8d7b8895f256279df84a8
LaiaRuizM <laia.ruizm@hotmail.com>
2023-08-01 16:17:28 +0200

    feat: add UberVan

--- Java/UberVan.java
+++ Java/UberVan.java
@@ -3,16 +3,17 @@ package Java;
 import java.util.ArrayList;
 import java.util.Map;
 
-class UberBlack extends Car {
+class UberVan extends Car {
     // We declare the attributes that are only UberBlack's attributes
     Map<String, Map<String, Integer>> typeCarAccepted;
     ArrayList<String> seatsMaterial;
 
 
     //todo Constructor method:
-    public UberBlack(String license, Account driver, Map<String, Map<String, Integer>> typeCarAccepted, ArrayList<String> seatsMaterial){
+    public UberVan(String license, Account driver, Map<String, Map<String, Integer>> typeCarAccepted, ArrayList<String> seatsMaterial){
         super(license, driver); // As if you called the constructor method of the car class and printDataCar method.
         this.typeCarAccepted = typeCarAccepted;
         this.seatsMaterial = seatsMaterial;
     }
 }
+
