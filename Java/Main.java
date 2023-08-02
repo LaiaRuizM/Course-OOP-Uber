f03197a316c7754f23f30e3313cdbba6507b7a96
LaiaRuizM <laia.ruizm@hotmail.com>
2023-08-02 17:42:48 +0200

    feat: change type car for uberX in Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -5,18 +5,18 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Good morning");
         Account account = new Account("Andrés Herrera", "AND123", "handres@gmail.com", "123456");
-        Car car = new Car("AMQ123", account);
+        UberX uberX = new UberX("AMQ123", account, "Chevrolet", "Sonic");
         //Car car = new Car(); // type of object + name of the object = new + constructor method
         //car.license = "AMQ123"; //licence, driver, etc are attributes
         //car.driver = "Andrés Herrera";
-        car.passenger = 4;
-        car.printDataCar(); 
+        uberX.passenger = 4;
+        uberX.printDataCar(); 
 
-        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876", "handrea@hotmail.com", "7890"));  
-        // car2.license = "QWE567";
-        // car2.driver = Andrea Herrera;
-        car2.passenger = 3;
-        car2.printDataCar();
+        // Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876", "handrea@hotmail.com", "7890"));  
+        // // car2.license = "QWE567";
+        // // car2.driver = Andrea Herrera;
+        // car2.passenger = 3;
+        // car2.printDataCar();
     }
 }
 
