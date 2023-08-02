b333b4ec1033ba7d264f1c96036f2b215df25c9a
LaiaRuizM <laia.ruizm@hotmail.com>
2023-08-02 19:13:38 +0200

    feat: generate setters and getters

--- Java/Car.java
+++ Java/Car.java
@@ -1,9 +1,9 @@
 package Java;
 
 class Car {
-    Integer id;
-    String license;
-    Account driver; //We need Account class bc we need different information about the driver such as name and document. 
+    private Integer id;
+    private String license;
+    private Account driver; //We need Account class bc we need different information about the driver such as name and document. 
     private Integer passenger; //Modifier of access: private -> because we only want this data (attribute) to be accessed by the class, this way the field car.passenger (in Main.java) won't be visible by Main class but it is visible by Car class (syso -> line 12 and 13 + run Main.java -> we can watch the passenger: 3).
 
     public Car(String license, Account driver){
@@ -31,6 +31,33 @@ class Car {
         }
     }
 
+
+    // rigth click + source action + generate setters and getters:
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
+    }
+
+    
     
 }
 
