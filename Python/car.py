8450e5dffca1209d0c6d8d084a8ac7eb186fa2dc
LaiaRuizM <laia.ruizm@hotmail.com>
2023-08-02 17:11:57 +0200

    feat: fix car.py error -> is now type Account with the mandatory data to instantiate an object of this type

--- Python/car.py
+++ Python/car.py
@@ -3,7 +3,7 @@ from account import Account # We import the class to be able to use it.
 class Car:
     id          = int
     license     = str
-    driver      = Account("","") # Driver -> is now type Account with the mandatory data to instantiate an object of this type.
+    driver      = Account("","", "", "") # Driver -> is now type Account with the mandatory data to instantiate an object of this type.
     passenger   = int
     
     def __init__(self, license, driver):
