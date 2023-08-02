fda29e546fe133387a869ed658aef695c5c000b6
LaiaRuizM <laia.ruizm@hotmail.com>
2023-08-02 15:51:58 +0200

    feat: fix Main.java error -> email and password are required in Account.java

--- Java/Main.java
+++ Java/Main.java
@@ -4,7 +4,7 @@ class Main {
     // two objects (car and car2) from car class:
     public static void main(String[] args) {
         System.out.println("Good morning");
-        Account account = new Account("Andrés Herrera", "AND123");
+        Account account = new Account("Andrés Herrera", "AND123", "handres@gmail.com", "123456");
         Car car = new Car("AMQ123", account);
         //Car car = new Car(); // type of object + name of the object = new + constructor method
         //car.license = "AMQ123"; //licence, driver, etc are attributes
@@ -12,7 +12,7 @@ class Main {
         car.passenger = 4;
         car.printDataCar(); 
 
-        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));  
+        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876", "handrea@hotmail.com", "7890"));  
         // car2.license = "QWE567";
         // car2.driver = Andrea Herrera;
         car2.passenger = 3;
