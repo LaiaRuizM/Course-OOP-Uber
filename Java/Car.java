b1cfa5f4ef67f504ce2046df8834de5419f0c18b
LaiaRuizM <laia.ruizm@hotmail.com>
2023-08-02 18:05:22 +0200

    feat: add encapsulation to Car.java -> private passenger

--- Java/Car.java
+++ Java/Car.java
@@ -4,11 +4,13 @@ class Car {
     Integer id;
     String license;
     Account driver; //We need Account class bc we need different information about the driver such as name and document. 
-    Integer passenger;
+    private Integer passenger; //Modifier of access: private -> because we only want this data (attribute) to be accessed by the class, this way the field car.passenger (in Main.java) won't be visible by Main class but it is visible by Car class (syso -> line 12 and 13 + run Main.java -> we can watch the passenger: 3).
 
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
+        passenger = 3;
+        System.out.println(" Passengers: " + passenger);
     }
 
     void printDataCar() {
--- Java/Main.java
+++ Java/Main.java
@@ -5,12 +5,12 @@ class Main {
         System.out.println("Good morning");
         Account account = new Account("Andrés Herrera", "AND123", "handres@gmail.com", "123456");
         UberX uberX = new UberX("AMQ123", account, "Chevrolet", "Sonic");
-        uberX.passenger = 4;
+        //uberX.passenger = 3; //An uberX can have 4 empty seats, not only 3, so we should encapsulate this information. We modificate this information in Car.java class adding an access modifier because there is where we have the passenger attribute.
         uberX.printDataCar(); 
 
-        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876", "handrea@hotmail.com", "7890"));  
-        car2.passenger = 3;
-        car2.printDataCar();
+        // Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876", "handrea@hotmail.com", "7890"));  
+        // car2.passenger = 3;
+        // car2.printDataCar();
     }
 }
 
